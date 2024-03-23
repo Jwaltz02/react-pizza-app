1fb41d741d03a7906c7fc67f8a71b0e79e38d109
Jwaltz02 <jermaine.walters0ii@gmail.com>
2024-03-22 20:09:45 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-waltz-pizza-app/precache-manifest.fc2de860f4bb0d9f35f4aa86c02eac92.js"
+  "/react-pizza-app/precache-manifest.bcedc0af58aa24e1c08b1dbbd80ee0cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-waltz-pizza-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pizza-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
